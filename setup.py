56f1b639b532b16224ba702da901ea4dab5518c3
ddolder <60158457+ddolder@users.noreply.github.com>
2020-10-20 11:16:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 
 setup(
     name=release_package,
-    version='0.0.2',
+    version='0.0.3',
     description='Water Quality - Utah lakes',
     long_description='',
     keywords='',
